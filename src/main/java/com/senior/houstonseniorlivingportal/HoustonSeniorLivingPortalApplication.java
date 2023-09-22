c5da77ffa875766e5dfcc04d9fc5e464a3079602
James OSullivan <james.a.osullivan01@gmail.com>
2023-09-22 11:43:23 -0500

    initializing project setup

---
+++ src/main/java/com/senior/houstonseniorlivingportal/HoustonSeniorLivingPortalApplication.java
@@ -0,0 +1,14 @@
+package com.senior.houstonseniorlivingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoustonSeniorLivingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoustonSeniorLivingPortalApplication.class, args);
+        System.out.println("Hello, Houston!");
+    }
+
+}
---
+++ src/test/java/com/senior/houstonseniorlivingportal/HoustonSeniorLivingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.houstonseniorlivingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoustonSeniorLivingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
